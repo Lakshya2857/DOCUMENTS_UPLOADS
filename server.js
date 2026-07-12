205d41a0794ccb0ad4be43b34181471f9c96ec88
Lakshya2857 <lakshyasangwan3111@gmail.com>
2026-07-12 17:12:04 +0530

    fix: explicitly set views directory for vercel

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(morgan("dev")); 
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // ROUTES
